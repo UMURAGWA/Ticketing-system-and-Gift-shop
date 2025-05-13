a26248966e7de211028aa0ff515b835e90a8bcc4
UMURAGWA <ishimwehertier4@gmail.com>
2025-05-13 10:08:52 +0200

    Updating home page

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+export default function Footer(){
+    return(
+        <footer className="bg-gray-900 text-white text-sm p-7 mt-8">
+        <div className="flex justify-around flex-wrap">
+        <Link href="/">Back to top</Link>
+        <Link href="/events">Events</Link>
+        <Link href="/shop">Gift Shop</Link>
+        <Link href="/Sign in">Sign In</Link>
+        <Link href="/Our contacts">Our Contacts</Link>
+        <Link href="/Our partners">Our Partners</Link>
+        <Link href="/Our social media">Our Social Media</Link>
+        </div>
+   
+        </footer>
+    )
+
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Link from "next/link";
+export default function Header(){
+    return(
+        <header  
+        className="flex flex-wrap justify-between items-center p-9 bg-gray-800 text-white">
+           
+            <div className="text-3xl font-bold"><Link href="/">😁 INKOMANE</Link></div>
+            <nav className="flex flex-wrap gap-4 text-sm">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/services">Services</Link>
+                <Link href="/shop">Shop</Link>
+                <Link href="/events">Event List</Link>
+                <Link href="/create-event">Create Event</Link>
+                <Link href="/pricing">Ticket Price</Link>
+                <Link href="/support">Help</Link>
+            </nav>
+            <div className="flex gap-4 text-sm">
+                <Link href="/signin">Sign in</Link>
+                <Link href="/contact">Contact</Link>
+                <Link href="/testimonials">Testimonials</Link>
+            </div>
+    </header>
+  );
+}
